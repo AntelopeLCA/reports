d84b0c49cdab390f6ec5d7f6f38034aaf94d7244
Brandon Kuczenski <brandon.kuczenski@301south.net>
2025-01-15 00:48:41 -0800

    Modernize results writer

--- antelope_reports/model_runner/results_writer.py
+++ antelope_reports/model_runner/results_writer.py
@@ -5,6 +5,9 @@ from antelope_reports.charts.pos_neg import PosNegCompareError
 
 
 class ResultsWriter(object):
+
+    pdf = True
+
     @property
     def unit_output(self):
         """
@@ -54,7 +57,7 @@ class ResultsWriter(object):
         return os.path.join(self.figs_path, '%s-lca-%s-bar%s.tex' % (self.scope, year, self.suffix))
 
     def pos_neg_eps(self, scenario):
-        return os.path.join(self.figs_path, '%s-pos-neg-%s%s.eps' % (self.scope, scenario, self.suffix))
+        return os.path.join(self.figs_path, '%s-pos-neg-%s%s.%s' % (self.scope, scenario, self.suffix, self._or_eps))
 
     def pos_neg_tex(self, scenario):
         return os.path.join(self.figs_path, '%s-pos-neg-%s%s.tex' % (self.scope, scenario, self.suffix))
@@ -71,11 +74,11 @@ class ResultsWriter(object):
 
     @property
     def lca_path(self):
-        return os.path.join(self.LCA_OUTPUT, self.scope)
+        return os.path.join(self.lca_root, self.scope)
 
     @property
     def figs_path(self):
-        return os.path.join(self.FIGS_PATH, self.scope)
+        return os.path.join(self.figs_root, self.scope)
 
     @property
     def suffix(self):
@@ -83,52 +86,65 @@ class ResultsWriter(object):
             return ''
         return '-%s' % self.case
 
-    def __init__(self, scope, output_path=None, figs_path=None, case=None):
+    def __init__(self, path, scope, lca_path='lca_output', figs_path='lca_figs', case=None):
         """
 
-        :param scope: a string used to construct the filenames
-        :param output_path: 'output'
-        :param figs_path: 'figs'
-        :param case: [None] '-'-joined file suffix before extension
+        :param path:
+        :param scope:
+        :param lca_path:
+        :param figs_path:
+        :param case:
         """
 
         self.scope = scope
         self.case = case
 
-        if output_path:
-            self.LCA_OUTPUT = os.path.abspath(output_path)
-        else:
-            self.LCA_OUTPUT = os.path.join(os.getcwd(), 'output')
-        if figs_path:
-            self.FIGS_PATH = os.path.abspath(figs_path)
-        else:
-            self.FIGS_PATH = os.path.join(os.getcwd(), 'figs')
+        self.lca_root = os.path.abspath(os.path.join(path, lca_path))
+        self.figs_root = os.path.abspath(os.path.join(path, figs_path))
 
         self._check_output_dir()
 
-    def generate_csv(self, study, scenario_order=None):
-        study.results_to_csv(self.full_output, scenarios=scenario_order)
+    def generate_csv(self, catra, **kwargs):
+        catra.results_to_csv(self.full_output, **kwargs)
 
     def generate_year_output(self, study_year, year, stage_order):
         # generate_pos_neg_compare(study_year)
         study_year.scenario_detail_tbl(year, filename=self.year_csv(year), column_order=stage_order)
 
-        study_year.results_to_tex(self.year_table(year), scenario=year, column_order=stage_order, format='%.2e', sort_column=0)
+        study_year.results_to_tex(self.year_table(year), scenario=year, column_order=stage_order, format='%.2e',
+                                  sort_column=0)
 
-    def generate_unit_output(self, study, scenario_order):
+    def generate_unit_output(self, study, scenario_order=None):
+        """
+        set display quantities via runner
+        :param study: LcaModelRunner instance
+        :param scenario_order:
+        :return:
+        """
         study.results_to_csv(self.unit_output, scenarios=scenario_order)
 
-        study.results_to_tex(self.unit_table, column_order=scenario_order, format='%.2e', sort_column=0)  # DISP_QS via runner
+        study.results_to_tex(self.unit_table, column_order=scenario_order, format='%.2e', sort_column=0)
 
     def pos_neg_chart(self, runner, scenario, qs=None, table=True):
         if qs is None:
             qs = list(runner.quantities)
         pn = PosNegCompareError(*(runner.sens_result(scenario, q) for q in qs), filename=self.pos_neg_eps(scenario))
         if table:
-            tabularx_ify(pn.dataframe, column_format='ll *{3}X', filename=self.pos_neg_tex(scenario), multicolumn=False)
+            # multicolumn=False is no longer supported
+            tabularx_ify(pn.dataframe, column_format='ll *{3}X',
+                         filename=self.pos_neg_tex(scenario))  # multicolumn=False
 
-    def activity_documenter(self, lca, *scenarios):
-        raise NotImplementedError
+    @property
+    def _or_png(self):
+        if self.pdf:
+            return 'pdf'
+        return 'png'
+
+    @property
+    def _or_eps(self):
+        if self.pdf:
+            return 'pdf'
+        return 'png'
 
     def __enter__(self):
         return self
